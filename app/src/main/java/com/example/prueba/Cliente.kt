290710d773ff40f1c49b4c326f1a649c90c25ccc
straiker321 <95499138+straiker321@users.noreply.github.com>
2022-06-21 01:35:08 -0500

    HOLA

---
+++ app/src/main/java/com/example/prueba/Cliente.kt
@@ -0,0 +1,37 @@
+package com.example.prueba
+
+
+//import com.google.gson.annotations.SerializedName
+//
+//data class Cliente(
+//    @SerializedName("apellido_mate")
+//    val apellidoMate: String,
+//    @SerializedName("apellido_pate")
+//    val apellidoPate: String,
+//    @SerializedName("celular")
+//    val celular: Int,
+//    @SerializedName("cod_cliente_dni")
+//    val codClienteDni: Int,
+//    @SerializedName("correo_elec")
+//    val correoElec: String,
+//    @SerializedName("departamento")
+//    val departamento: String,
+//    @SerializedName("direccion")
+//    val direccion: String,
+//    @SerializedName("distrito")
+//    val distrito: String,
+//    @SerializedName("dni")
+//    val dni: String,
+//    @SerializedName("estado_civil")
+//    val estadoCivil: String,
+//    @SerializedName("fecha_afiliacion")
+//    val fechaAfiliacion: String,
+//    @SerializedName("fecha_naci")
+//    val fechaNaci: String,
+//    @SerializedName("nombre")
+//    val nombre: String,
+//    @SerializedName("ocupacion")
+//    val ocupacion: String,
+//    @SerializedName("provincia")
+//    val provincia: String
+//)
---
+++ app/src/main/java/com/example/prueba/api/ApiCliente.kt
@@ -0,0 +1,10 @@
+package com.example.prueba.api
+
+//import com.example.prueba_consumo.entity.Cliente
+//import retrofit2.Call
+//import retrofit2.http.GET
+//
+//interface ApiCliente {
+//    @GET("cliente/")
+//    fun getAllPosts(): Call<List<Cliente>>
+//}
