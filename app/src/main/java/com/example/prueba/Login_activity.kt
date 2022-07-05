ab07288230429e2b717dad06d1acb2c39608ef50
straiker321 <95499138+straiker321@users.noreply.github.com>
2022-07-05 12:34:21 -0500

    HOLA

--- app/src/main/java/com/example/prueba/Login_activity.kt
+++ app/src/main/java/com/example/prueba/Login_activity.kt
@@ -7,56 +7,55 @@ import android.widget.Button
 import android.widget.Toast
 import com.example.prueba.databinding.ActivityLoginBinding
 import com.example.prueba.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Login_activity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        val btn_inicar=findViewById<Button>(R.id.btn_inicar)
-        btn_inicar.setOnClickListener{
-            val lanzar = Intent(this,MainActivity::class.java)
-            startActivity(lanzar)
-        }
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        val btn_inicar=findViewById<Button>(R.id.btn_inicar)
+//        btn_inicar.setOnClickListener{
+//            val lanzar = Intent(this,MainActivity::class.java)
+//            startActivity(lanzar)
+//        }
 //        val btn_inicar=findViewById<Button>(R.id.btn_inicar)
 //        btn_inicar.setOnClickListener{
 //            val lanzar = Intent(this,Peliculas_activity::class.java)
 //            startActivity(lanzar)
 //        }
-        val btn_registrar=findViewById<Button>(R.id.btn_registrar)
-        btn_registrar.setOnClickListener{
-            val lanzar2 = Intent(this,Register_activity::class.java)
-            startActivity(lanzar2)
+//        val btn_registrar=findViewById<Button>(R.id.btn_registrar)
+//        btn_registrar.setOnClickListener{
+//            val lanzar2 = Intent(this,Register_activity::class.java)
+//            startActivity(lanzar2)
+//        }
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnRegistrar.setOnClickListener {
+            val intent = Intent(this, Register_activity::class.java)
+            startActivity(intent)
         }
+        binding.btnInicar.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                    firebaseAuth.signInWithEmailAndPassword(email , password).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent = Intent(this, Register_activity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString(),Toast.LENGTH_SHORT).show()
+                        }
+                    }
 
-
-//        firebaseAuth = FirebaseAuth.getInstance()
-//        binding.btnRegistrar.setOnClickListener {
-//            val intent = Intent(this, ActivityRegisterBinding::class.java)
-//            startActivity(intent)
-//        }
-//        binding.btnInicar.setOnClickListener {
-//            val email = binding.email.text.toString()
-//            val password = binding.password.text.toString()
-//            val pass = binding.confirmPass.text.toString()
-//            if(email.isNotEmpty() && password.isNotEmpty() && pass.isNotEmpty()){
-//                if(password == pass){
-//                    firebaseAuth.createUserWithEmailAndPassword(email , password).addOnCompleteListener {
-//                        if (it.isSuccessful){
-//                            val intent = Intent(this, ActivityRegisterBinding::class.java)
-//                            startActivity(intent)
-//                        }else{
-//                            Toast.makeText(this, it.exception.toString(),Toast.LENGTH_SHORT).show()
-//                        }
-//                    }
-//
-//                }else{
-//                    Toast.makeText(this,"pass no encontrada", Toast.LENGTH_SHORT).show()
-//                }
-//            }else{
-//                Toast.makeText(this,"empty fields are not Allowed !!", Toast.LENGTH_SHORT).show()
-//            }
-//        }
+            }else{
+                Toast.makeText(this,"empty fields are not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/prueba/Register_activity.kt
+++ app/src/main/java/com/example/prueba/Register_activity.kt
@@ -8,50 +8,52 @@ import android.widget.Toast
 import com.example.prueba.databinding.ActivityLoginBinding
 import com.example.prueba.databinding.ActivityRegisterBinding
 import com.example.prueba.databinding.ActivityRegisterSiguiente2Binding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Register_activity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_siguiente)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        firebaseAuth = FirebaseAuth.getInstance()
-//        binding.btnRegistrar.setOnClickListener {
-//            val intent = Intent(this, ActivityLoginBinding::class.java)
-//            startActivity(intent)
-//        }
-//
-//        binding.btnRegistrar.setOnClickListener {
-//            val corr = binding.editTextTextPersonName6.text.toString()
-//            val password = binding.editTextTextPersonName7.text.toString()
-//            if (corr.isNotEmpty() && password.isNotEmpty()) {
-//
-//                firebaseAuth.signInWithEmailAndPassword(corr, password).addOnCompleteListener {
-//                    if (it.isSuccessful) {
-//                        val intent = Intent(this, MainActivity::class.java)
-//                        startActivity(intent)
-//                    } else {
-//                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//            } else {
-//                Toast.makeText(this, "empty fields are not Allowed !!", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//
-//        override fun onStart() {
-//            super.onStart()
-//
-//            if (firebaseAuth.currentUser != null) {
-//                val intent = Intent(this, MainActivity::class.java)
-//                startActivity(intent)
-//            }
-//        }
-        val btn_siguiente=findViewById<Button>(R.id.btn_siguiente2)
-        btn_siguiente.setOnClickListener{
-            val lanzar2 = Intent(this,Register_siguiente::class.java)
-            startActivity(lanzar2)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnRegistro.setOnClickListener {
+            val intent = Intent(this, Login_activity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegistro.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.pass.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "empty fields are not Allowed !!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
-}
+        override fun onStart() {
+            super.onStart()
+
+            if (firebaseAuth.currentUser != null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+//        val btn_siguiente=findViewById<Button>(R.id.btn_siguiente2)
+//        btn_siguiente.setOnClickListener{
+//            val lanzar2 = Intent(this,Register_siguiente::class.java)
+//            startActivity(lanzar2)
+//        }
+    }
